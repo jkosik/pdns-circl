9f2157221fe017ac3bd1f5ba7cb9eaee8a53c276
Juraj Kosik <juraj.kosik@gmail.com>
2018-11-20 14:38:30 +0100

    poc

--- pdns-circl.go
+++ pdns-circl.go
@@ -5,75 +5,28 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
 )
 
-type Pdns struct {
-	Count      int    `json:"count"`
-	Origin     string `json:"origin"`
+type PdnsRecord struct {
+	Count     int    `json:"count"`
+	Origin    string `json:"origin"`
 	TimeFirst int    `json:"time_first"`
-	RRType     string `json:"rrtype"`
-	RRName     string `json:"rrname"`
-	RData      string `json:"rdata"`
+	RRType    string `json:"rrtype"`
+	RRName    string `json:"rrname"`
+	RData     string `json:"rdata"`
 	TimeLast  int    `json:"time_last"`
 }
 
-type Record struct {
-	Record	string
-}
+var username = flag.String("u", "foo", "Username")
+var password = flag.String("p", "bar", "Password")
+var rrName = flag.String("rrname", "www.circl.lu", "Domain to lookup, e.g. www.google.com")
+var interactive = flag.Bool("i", false, "Interactive Mode")
 
-
-===========================
-var input = `
-{"foo": "bar", "aaa":123}
-{"foo": "baz", "aaa":222}
-`
-
-type Doc struct {
-	Foo string
-	Aaa int
-}
-
-func main() {
-	dec := json.NewDecoder(strings.NewReader(input))
-	for {
-		var doc Doc
-
-		err := dec.Decode(&doc)
-		if err == io.EOF {
-			// all done
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%+v\n", doc)
-	}
-}
-
-
-================
-
-
-
-
-func main() {
-
-	username := flag.String("u", "foo", "Username")
-	password := flag.String("p", "bar", "Password")
-	rrName := flag.String("rrname", "www.circl.lu", "Domain to lookup, e.g. www.google.com")
-	//	rData := flag.String("rdata", "cpa.circl.lu", "Data in response")
-	flag.Parse()
-
-	var urlConcat bytes.Buffer
-	urlConcat.WriteString("https://www.circl.lu/pdns/query/")
-	urlConcat.WriteString(*rrName)
-	url := urlConcat.String()
-	fmt.Println(url)
+func callAPI(url string) []byte {
 	netClient := &http.Client{
 		Timeout: time.Second * 10,
 	}
@@ -91,40 +44,57 @@ func main() {
 		panic(resErr.Error())
 	}
 
-	body, readErr := io.ioutil.ReadAll(res.Body)
+	body, readErr := ioutil.ReadAll(res.Body)
 	if readErr != nil {
 		panic(readErr.Error())
 	}
-	fmt.Println(body)
-
-	//content := Pdns{}
-	//content := map[string]Pdns{}
-	content := Record{}
-	fmt.Println("before")
-	fmt.Println(content)
-	unmarshalErr := json.Unmarshal(body, &content)
-	if unmarshalErr != nil {
-		panic(unmarshalErr.Error())
-	}
-	fmt.Println("after")
-	fmt.Println(content)
-
-	marshal, marshalErr := json.MarshalIndent(content, "", "  ")
-	if marshalErr != nil {
-		panic(marshalErr.Error())
-	}
-	fmt.Println(string(marshal))
+	return body
 }
 
-/*
-//http://blog.josephmisiti.com/parsing-json-responses-in-golang
-func getCapec(body []byte) (*Capec, error) {
-	var s = new(Capec)
-	err := json.Unmarshal(body, &s)
-	if(err != nil){
-	        fmt.Println("whoops:", err)
-	}
-	return s, err
-}
+func main() {
 
-*/
+	flag.Parse()
+	var urlConcat bytes.Buffer
+	urlConcat.WriteString("https://www.circl.lu/pdns/query/")
+	urlConcat.WriteString(*rrName)
+	url := urlConcat.String()
+
+	body := callAPI(url)
+
+	//API response is a list of JSONs not one JSON object. Prior unmarshaling it is needed to fix JSON format
+	s := string(body[:]) //byte array to string
+
+	dataRaw := strings.Split(s, "\n") //slice from multiline string
+	data := dataRaw[:len(dataRaw)-1]  //delete last empty line
+
+	//Adding JSON delimiters and comma separation
+	var buffer bytes.Buffer
+	buffer.WriteString("[") //starting JSON char
+	for i, rec := range data {
+		buffer.WriteString(rec) //trailing JSON char
+		if i < len(data)-1 {    //add comma delimiter except last iteration
+			buffer.WriteString(",")
+		}
+	}
+	buffer.WriteString("]")     //trailing JSON char
+	jsonData := buffer.String() //final valid JSON string ready for unmarshaling
+	//fmt.Println("jsonData:", jsonData)
+	var records []PdnsRecord
+	err := json.Unmarshal([]byte(jsonData), &records)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if *interactive == false {
+		fmt.Println(jsonData)
+	} else {
+		fmt.Println("Running interactive mode for", url)
+       		fmt.Print("Enter First String: ")   //Print function is used to display output in same line
+		var rrtype string    
+	        fmt.Scanln(&rrtype) 
+		fmt.Println("rrtype:",rrtype)
+	}
+
+	//	fmt.Println(records[0].Count)
+
+}
